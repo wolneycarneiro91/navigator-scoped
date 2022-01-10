a8910b1cda33c0d74c045e8f2f587a9b4acb6a1e
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2022-01-10 14:30:13 -0300

    [Adicionando actions ignorando runner

--- .github/actions/laravel-build/hmg-entrypoint.sh
+++ .github/actions/laravel-build/hmg-entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-echo homologacao
+cd ../hmg/navigator-scoped
+git pull
+./vendor/bin/sail up
+php artisan optimize:clear
+php artisan key:generate
+php artisan migrate
+chmod -R 777 storage bootstrap/cache
--- .github/actions/laravel-build/prd-entrypoint.sh
+++ .github/actions/laravel-build/prd-entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-echo producao
+cd ../prd/navigator-scoped
+git pull
+./vendor/bin/sail up
+php artisan optimize:clear
+php artisan key:generate
+php artisan migrate
+chmod -R 777 storage bootstrap/cache
