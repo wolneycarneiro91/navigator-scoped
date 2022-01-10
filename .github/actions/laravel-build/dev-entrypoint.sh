0d13b074be6568a5bcd55748b2bc952007c4ffb5
wolneycarneiro91 <wolneycarneiro91@gmail.com>
2022-01-10 15:42:34 -0300

    sail up

--- .github/actions/laravel-build/dev-entrypoint.sh
+++ .github/actions/laravel-build/dev-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home/wolney/projetos/desenvolvimento_navigator_scoped/navigator-scoped  
 git pull
+./vendor/bin/sail up
 php artisan optimize:clear
 php artisan key:generate
 php artisan migrate
